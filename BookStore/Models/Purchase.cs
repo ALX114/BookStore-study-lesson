b61f13580d3c995435e73aaf15fd4dc0efe6f066
Копытов Алексей <coopchanel@gmail.com>
2020-07-23 12:10:10 +0500

    Добавьте файлы проекта.

---
+++ BookStore/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId {set; get;}
+        public string Person { set; get; }
+        public string Address { set; get; }
+        public int BookId { set; get; }
+        public DateTime Date { set; get; }
+    }
+}
